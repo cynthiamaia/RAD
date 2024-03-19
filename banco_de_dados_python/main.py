082e685dcad397fb6b6efcbb1c3285ee8ed0bb51
cynthiamaia <cynthiamaia45@gmail.com>
2024-03-19 08:16:39 -0300

    new

--- banco_de_dados_python/main.py
+++ banco_de_dados_python/main.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from modelo import Pessoa, Marca, Veiculo
 banco = sqlite3.connect('database.db')
 banco.execute("PRAGMA foreign_keys=on")
 cursor = banco.cursor()
@@ -24,3 +24,43 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Veiculo(
                 PRIMARY KEY(placa),
                 FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
                 FOREIGN KEY(marca) REFERENCES Marca(id));''')
+
+"""cursor.execute('''ALTER TABLE 
+               Veiculo ADD motor 
+               REAL;''')"""
+
+"""
+###uma das formas
+#criar uma classe Pessoa
+#criar objeto do tipo Pessoa
+pessoa = Pessoa(92345678900, 
+                'Pedro', '2000-01-31',
+                True)
+#Inserção de dados em tabela com query dinâmicas
+comando = '''INSERT INTO Pessoa 
+(cpf, nome, nascimento, oculos) 
+VALUES (?, ?,?,?);''' #delimitador “?”
+cursor.execute(comando, (pessoa.cpf,
+                         pessoa.nome, 
+                         pessoa.nascimento,
+                         pessoa.usa_oculos))
+
+"""
+"""
+###
+##Outra forma
+# Cria uma lista de objetos Pessoa
+pessoas = [
+    Pessoa(12345678900, 'João', '2000-01-31', True),
+    Pessoa(98765432100, 'Cynthia', '1995-03-10', False)
+]
+
+# Inserção de dados em tabela com query dinâmicas usando executemany
+comando = '''INSERT INTO Pessoa (cpf, nome, 
+nascimento, oculos) VALUES (?, ?,?,?);'''
+cursor.executemany(comando,
+                   [(i.cpf, i.nome, 
+                    i.nascimento, 
+                    i.usa_oculos) 
+                    for i in pessoas])
+"""
