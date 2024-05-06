646f5552e07aa720bf60249f14dd1d18deb98e2e
cynthiamaia <cynthiamaia45@gmail.com>
2024-05-06 14:12:33 -0300

    flask_news__

--- Aula_flask/app/__init__.py
+++ Aula_flask/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api 
 app = Flask(__name__)
+api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app)
 
