e7187e31230b417908c7b21738552fd0c03e83c1
cynthiamaia <cynthiamaia45@gmail.com>
2024-05-07 20:45:09 -0300

    flask__news__

--- Aula_flask/app/__init__.py
+++ Aula_flask/app/__init__.py
@@ -1,18 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api 
 app = Flask(__name__)
-api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app)
 
-from app.models.products import Products
-with app.app_context(): 
-    db.create_all()
-
-from app.view.reso_products import Index,ProductCreate
-api.add_resource(Index, '/')
-api.add_resource(ProductCreate, '/criar')
 '''@app.route("/")
 def index():
     #return "<h1> Minha Aplicação em Flask</h1>"
