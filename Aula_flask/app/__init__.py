2cad831da4c27a8b25ce74b4cefb8c38a0d9c113
cynthiamaia <cynthiamaia45@gmail.com>
2024-05-20 14:39:39 -0300

    flask___news____

--- Aula_flask/app/__init__.py
+++ Aula_flask/app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy 
 from flask_restful import Api 
-from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
 #configuração com banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud4.db'
